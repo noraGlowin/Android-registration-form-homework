acb1623ede6aa393991ae9b5898734184c27ae0f
Nora <Norah.Nahedh.ruh.k@tuwaiq.edu.sa>
2021-11-05 12:23:52 +0300

    homework Correction

--- app/src/main/java/com/example/androidregistrationformhomework/MainActivity.kt
+++ app/src/main/java/com/example/androidregistrationformhomework/MainActivity.kt
@@ -4,29 +4,34 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidregistrationformhomework.databinding.ActivityMainBinding
-
+import android.util.Patterns
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.registration.setOnClickListener { registration() }
     }
     fun registration() {
-        val userName = binding.name.text.toString()
-        val userEmail = binding.email.text.toString()
-        val passWord = binding.password.text.toString()
-        val rePassWord = binding.rePassword.text.toString()
-        val birthDay = binding.birtday.text.toString()
+        val userName = binding.nameInput.text.toString()
+        val userEmail = binding.emailInput.text.toString()
+        val passWord = binding.passwordInput.text.toString()
+        val rePassWord = binding.repassInput.text.toString()
+        val birthDay = binding.birthdayInput.text.toString()
         val selectGender = binding.gender.checkedRadioButtonId
         val gender = when (selectGender) {
             R.id.female -> "Female"
-            R.id.male -> "male"
-            else -> "you are an angle"
+            else -> "male"
+
         }
         if (userName.isNotEmpty() && userEmail.isNotEmpty() && passWord.isNotEmpty() && rePassWord.isNotEmpty()){
-            if (passWord == rePassWord) {
+            if (!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches())
+            {
+                binding.emailContainer.helperText = "Invalid Email Address"
+            }
+           else if (passWord == rePassWord) {
 
                 binding.userInfo.setText("Profile \n " +
                         "Name: $userName \n " +
@@ -37,13 +42,24 @@ class MainActivity : AppCompatActivity() {
                         "Re-Password : $rePassWord ")
 
             } else {
-                binding.userInfo.setText("Password Missmatch , please re-enter your password")
+                binding.passwordContainer.helperText="Password Mismatch , please re-enter your password"
+                binding.repassContainer.helperText="Password Mismatch , please re-enter your password"
             }
         }else{
-            binding.warning.setText("full the blank please !")
+            binding.nameContainer.helperText="full the blank please !"
+            binding.emailContainer.helperText="full the blank please !"
+            binding.passwordContainer.helperText="full the blank please !"
+            binding.repassContainer.helperText="full the blank please !"
+            binding.birthdayContainer.helperText="full the blank please !"
         }
 
         }
-
+//    public final static boolean isValidEmail(CharSequence target) {
+//        if (target == null) {
+//            return false;
+//        } else {
+//            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+//        }
+//    }
 
 }
